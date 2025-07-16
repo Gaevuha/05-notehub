117276a7ca8349aa37dc33d072b7e8fe46681e6d
gaevuha <gaevuha@gmail.com>
2025-07-16 21:50:27 +0300

    add task05

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+import css from './App.module.css'
+
+
+export default function App() {
+return (
+    <div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+
+  );
+}
